633d973ffdb88b57f05936c9d85ea3bc19f307e0
Ryan S. Brown <sb@ryansb.com>
2015-04-29 18:16:17 -0400

    Add a time-based rolling counter to track messages on a per-node basis

---
+++ disq/rolling_counter.py
@@ -0,0 +1,84 @@
+import time
+from collections import defaultdict
+
+import six
+from blist import sortedlist
+
+
+class RollingCounter(object):
+    """
+    The argument to RollingCounter (ttl_secs) indicates how long each event
+    should count towards the total for its key, the default is 10 seconds, but
+    you may use any float to indicate how long (in seconds) an event should
+    stay in the count.
+
+    Example:
+        rc = RollingCounter(ttl_secs=0.5)
+        rc.add('foo')
+        rc.max() # 'foo'
+        time.sleep(1)
+        rc.max() # None
+        rc.keys() # []
+
+    Usage:
+        use .add('itemname') to increment a count for some id
+    """
+    def __init__(self, ttl_secs=10):
+        self._counts = defaultdict(sortedlist)
+        self._ttl_seconds = ttl_secs
+
+    def add(self, id):
+        self._counts[id].add(time.time())
+
+    def max(self):
+        r = self._rank(reverse=True)
+        if len(r):
+            return r[0]
+        return None
+
+    def min(self):
+        r = self._rank(reverse=False)
+        if len(r):
+            return r[0]
+        return None
+
+    def ranked(self):
+        self._expire()
+        return [
+            (x[0], len(x[1])) for x in sorted(
+                six.iteritems(self._counts),
+                key=lambda x: len(x[1])
+            )
+        ]
+
+    def count(self, id):
+        self._expire()
+        return len(self._counts[id])
+
+    def _rank(self, reverse=False):
+        self._expire()
+        return [
+            x[0] for x in sorted(
+                six.iteritems(self._counts),
+                key=lambda x: len(x[1]),
+                reverse=reverse
+            )
+        ]
+
+    def _expire(self):
+        # cast key iterable to list because this loop can delete keys
+        for k in list(six.iterkeys(self._counts)):
+            # find the location where all times are less than (current - ttl)
+            # and delete all lesser elements
+            del self._counts[k][
+                :self._counts[k].bisect(time.time() - self._ttl_seconds)
+            ]
+            if len(self._counts[k]) == 0:
+                self.remove(k)
+
+    def remove(self, id):
+        del self._counts[id]
+
+    def keys(self):
+        self._expire()
+        return list(six.iterkeys(self._counts))
---
+++ tests/test_rolling_counter.py
@@ -0,0 +1,42 @@
+# Copyright 2015 Ryan Brown <sb@ryansb.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+import time
+from disq.rolling_counter import RollingCounter
+
+
+class TestRollingCounter(object):
+    def test_rank(self):
+        rc = RollingCounter()
+        for _ in range(100):
+            rc.add('foo')
+        for _ in range(10):
+            rc.add('bar')
+        for _ in range(40):
+            rc.add('baz')
+        for _ in range(60):
+            rc.add('quux')
+        assert rc.max() == 'foo'
+        assert rc.min() == 'bar'
+
+    def test_expiration(self):
+        rc = RollingCounter(ttl_secs=0.5)
+        for _ in range(10):
+            rc.add('foo')
+        for _ in range(5):
+            rc.add('bar')
+        assert len(rc.keys()) == 2
+        time.sleep(1)
+        assert len(rc.keys()) == 0
